299dce9a3f5358225c639bd4486afc5ff44ab757
KessyRamos <kessyellenramos@gmail.com>
2022-06-05 09:47:45 -0300

    PrimeiroCommit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿double h = 1.90;
+double pesoIdeal = (72.7 * h) -58;
+Console.WriteLine("Calcular o peso ideal de um homem!!!");
+
+Console.Write($"O peso ideal do homem de acordo com a altura é: {pesoIdeal} Kg");
